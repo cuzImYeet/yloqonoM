1b58fd987cc8c25ce044e9fab89861d3a90d32ea
XxcyanOPxX <andreas_grassl@hotmail.com>
2025-05-21 12:07:44 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
 public class Main 
 {
-    public static void main(String[] args)   {Spielfeld brett = new Spielfeld ();}//// erzeugen eines Spielbretts 
+    public static void main(String[] args)   {Spielfeld brett = new Spielfeld ();}// erzeugen eines Spielbretts 
 }
